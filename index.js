6c9bb9df2e76de1468cb8104ac234f8910647516
Jeff Long <banterability@users.noreply.github.com>
2020-05-26 15:41:42 -0500

    Finalize initial public version

--- index.js
+++ index.js
@@ -1,67 +1,54 @@
-const debug = require("debug")("little-print");
 const https = require("https");
 const VERSION = require("./package.json").version;
 
 class LittlePrint {
-  constructor({appName, deviceKey}) {
-    this.appName = appName || 'LittlePrint';
+  constructor({ appName, deviceKey }) {
+    this.appName = appName || "LittlePrint";
 
     if (!deviceKey) throw new Error("deviceKey required");
-
     this.deviceKey = deviceKey;
   }
 
-  html(data) {
+  printHTML(data) {
     this._makeRequest(data, "html");
   }
 
-  status() {
-    this._makeRequest(null, null, "GET")
-  }
-
-  text(data) {
+  printText(data) {
     this._makeRequest(data, "text");
   }
 
-  _makeRequest(data, type, method="POST") {
+  _makeRequest(data, type) {
     const payload = { [type]: data };
 
     const body = JSON.stringify(payload);
 
-    debug('payload', body);
+    console.log(body);
 
     const options = {
       hostname: "device.li",
       port: 443,
       path: `/${this.deviceKey}?from=${encodeURIComponent(this.appName)}`,
-      method: method,
+      method: "POST",
       headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Content-Type": "application/json; charset=utf-8",
         "Content-Length": body.length,
-        "User-Agent": `little-print/v${VERSION}`
-      }
+        "User-Agent": `little-print/v${VERSION}`,
+      },
     };
 
-    debug('options', options);
-
-    const req = https.request(options, res => {
-      debug(`status: ${res.statusCode}`);
-
-      res.on("data", d => {
-        debug('on data');
+    const req = https.request(options, (res) => {
+      res.on("data", (d) => {
         process.stdout.write(d);
       });
     });
 
-    req.on("error", error => {
-      debug('on error');
+    req.on("error", (error) => {
       console.error(error);
     });
 
     req.write(body);
     req.end();
-    debug('end');
   }
 }
 
