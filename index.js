c68f99b622c93d96e9930c5558ae6537d5b13650
Jeff Long <banterability@users.noreply.github.com>
2023-03-11 12:54:45 -0600

    extract mime

--- index.js
+++ index.js
@@ -1,35 +1,8 @@
 import { readFileSync } from "node:fs";
 import https from "node:https";
-import { extname } from "node:path";
+import { getMimeType } from "./lib/mimeType.js";
 
 const VERSION = "2.0.0";
-
-const MIME_TYPES = Object.freeze({
-  png: "image/png",
-  gif: "image/gif",
-  jpg: "image/jpeg",
-});
-
-const EXTENTIONS = Object.freeze({
-  ".gif": MIME_TYPES["gif"],
-  ".jpg": MIME_TYPES["jpg"],
-  ".jpeg": MIME_TYPES["jpg"],
-  ".png": MIME_TYPES["png"],
-});
-
-function getMimeType(path) {
-  const extention = extname(path);
-  const mimeType = EXTENTIONS[extention];
-  console.log({ extention, mimeType });
-  if (!mimeType) {
-    throw new Error(
-      `Unsupported file type. Expected one of [${Object.values(MIME_TYPES).join(
-        ", "
-      )}]`
-    );
-  }
-  return mimeType;
-}
 
 export default class LittlePrint {
   constructor({ appName, deviceKey }) {
